79cd649b1ec43503580516017529e44217993165
Brad Brown <brad.brown@rackspace.com>
2016-12-03 15:29:43 -0600

    Fix: I hate bash

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ echo "Brad's Auto Setup: preparing to set up your Mac..."
 
 # check for brew, install if not
 echo "Checking for homebrew..."
-command -v brew >/dev/null 2>&1 || {echo "Brew not installed, installing now..." >&2; /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";}
+command -v brew >/dev/null 2>&1 || {/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"}
 
 # add all brew requirements
 echo "brew installing all required modules..."
@@ -17,23 +17,24 @@ done
 
 # setup vim
 echo "Installing Plug for vim..."
-if ! [-e ~/.vim/autoload/plug.vim]
+if ! [ -e ~/.vim/autoload/plug.vim ]
 then
-    cd ~/.vim/autoload
-    git clone https://gitub.com/junegunn/vim-plug.git
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
 # Move old Bash Profile and vimrc, create link to new ones
 echo "Moving old bash profile and vimrc, symlinking dotfiles versions..."
-if [-e ~/.bash_profile]
-then
-    mv ~/.bash_profile ~/.bash_profile_old
+if ! [ -s ~/.bash_profile ]; then
+    if [ -e ~/.bash_profile ]; then
+        mv ~/.bash_profile ~/.bash_profile_old
+    fi
+    ln -s ~/dotfiles/bash_profile ~/.bash_profile
 fi
-ln -s ~/dotfiles/bash_profile ~/.bash_profile
-if [-e ~/.vimrc]
-then
-    mv ~/.vimrc ~/.vimrc_old
+if ! [ -s ~/.vimrc ]; then
+    if [ -e ~/.vimrc ]; then
+        mv ~/.vimrc ~/.vimrc_old
+    fi
+    ln -s ~/dotfiles/vimrc ~/.vimrc
 fi
-ln -s ~/dotfiles/vimrc ~/.vimrc
 
 echo "All set, Enjoy your mac!"
